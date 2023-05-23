0f4daab0b3d80d71f9d3ee05b000fe3d3e404a1b
PhilippeAmorim <65184037+PhilippeAmorim@users.noreply.github.com>
2023-05-23 10:38:34 -0300

    Add files via upload

---
+++ Gráfico de Rosca/GraficoDeRosca/generated/ManifestTypes.d.ts
@@ -0,0 +1,15 @@
+/*
+*This is auto generated from the ControlManifest.Input.xml file
+*/
+
+// Define IInputs and IOutputs Type. They should match with ControlManifest.
+export interface IInputs {
+    Valor: ComponentFramework.PropertyTypes.DecimalNumberProperty;
+    CorRosca: ComponentFramework.PropertyTypes.StringProperty;
+    CorTexto: ComponentFramework.PropertyTypes.StringProperty;
+}
+export interface IOutputs {
+    Valor?: number;
+    CorRosca?: string;
+    CorTexto?: string;
+}
