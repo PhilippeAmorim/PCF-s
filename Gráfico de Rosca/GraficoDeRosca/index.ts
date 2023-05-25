959673b093121d38dfb474df402d72601cb0ddb5
PhilippeAmorim <65184037+PhilippeAmorim@users.noreply.github.com>
2023-05-25 10:21:16 -0300

    Adicionando contra cor ao gráfico de rosca

--- Gráfico de Rosca/GraficoDeRosca/index.ts
+++ Gráfico de Rosca/GraficoDeRosca/index.ts
@@ -5,6 +5,7 @@ export class GraficoDeRosca implements ComponentFramework.StandardControl<IInput
   private _container: HTMLDivElement;
   private _svg: SVGElement;
   private _circle: SVGCircleElement;
+  private _backgroundCircle: SVGCircleElement; // Novo elemento para o círculo de destaque
   private _text: SVGTextElement;
 
   constructor() {
@@ -20,7 +21,16 @@ export class GraficoDeRosca implements ComponentFramework.StandardControl<IInput
     this._svg.style.width = "100%";
     this._svg.style.height = "100%";
 
-    // Criação do círculo de fundo
+    // Criação do círculo de fundo (rosquinha)
+    this._backgroundCircle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+    this._backgroundCircle.setAttribute("cx", "50");
+    this._backgroundCircle.setAttribute("cy", "50");
+    this._backgroundCircle.setAttribute("r", "40"); // Raio maior que o círculo principal
+    this._backgroundCircle.setAttribute("fill", "none"); // Cor de destaque (exemplo: #f5f5f5")
+    this._backgroundCircle.setAttribute("stroke", "#f5f5f5"); // Sem contorno
+    this._backgroundCircle.setAttribute("stroke-width", "10");
+
+    // Criação do círculo principal
     this._circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
     this._circle.setAttribute("cx", "50");
     this._circle.setAttribute("cy", "50");
@@ -40,6 +50,7 @@ export class GraficoDeRosca implements ComponentFramework.StandardControl<IInput
     this._text.setAttribute("fill", "#333");
 
     // Adicionar elementos SVG ao contêiner
+    this._svg.appendChild(this._backgroundCircle); // Adicionar o círculo de destaque primeiro
     this._svg.appendChild(this._circle);
     this._svg.appendChild(this._text);
     this._container.appendChild(this._svg);
@@ -47,8 +58,9 @@ export class GraficoDeRosca implements ComponentFramework.StandardControl<IInput
 
   public updateView(context: ComponentFramework.Context<IInputs>): void {
     const valor = context.parameters.Valor.raw || 0; // Obter o valor da propriedade "Valor"
-    const corRosca = context.parameters.CorRosca.raw || "#ddd"; // Obter o valor da propriedade "CorRosca"
-    const corTexto = context.parameters.CorTexto.raw || "#333"; // Obter o valor da propriedade "CorTexto"
+    const corRosca = context.parameters.CorRosca.raw || "#000"; // Obter o valor da propriedade "CorRosca"
+    const corTexto = context.parameters.CorTexto.raw || "#000"; // Obter o valor da propriedade "CorTexto"
+    const contracor = context.parameters.ContraCor.raw || "#f5f5f5"; // Obter o valor da propriedade "CorTexto"
 
     // Calcular a porcentagem com base no valor (assumindo que o valor varia de 0 a 100)
     const porcentagem = Math.min(Math.max(valor, 0), 100);
@@ -62,6 +74,9 @@ export class GraficoDeRosca implements ComponentFramework.StandardControl<IInput
 
     // Atualizar a cor do círculo
     this._circle.setAttribute("stroke", corRosca);
+
+    // Atualizar a cor do círculo de destaque
+    this._backgroundCircle.setAttribute("stroke", contracor); // Alterar a cor de destaque conforme necessário
 
     // Atualizar a cor do texto
     this._text.setAttribute("fill", corTexto);
