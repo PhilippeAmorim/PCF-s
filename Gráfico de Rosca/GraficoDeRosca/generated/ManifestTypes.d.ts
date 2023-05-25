9c6c2934c8028dec5196bc2fe8a46c057dd3cbfd
PhilippeAmorim <65184037+PhilippeAmorim@users.noreply.github.com>
2023-05-25 10:21:56 -0300

    Adicionando propriedade contra cor

--- Gráfico de Rosca/GraficoDeRosca/generated/ManifestTypes.d.ts
+++ Gráfico de Rosca/GraficoDeRosca/generated/ManifestTypes.d.ts
@@ -6,10 +6,12 @@
 export interface IInputs {
     Valor: ComponentFramework.PropertyTypes.DecimalNumberProperty;
     CorRosca: ComponentFramework.PropertyTypes.StringProperty;
+    ContraCor: ComponentFramework.PropertyTypes.StringProperty;
     CorTexto: ComponentFramework.PropertyTypes.StringProperty;
 }
 export interface IOutputs {
     Valor?: number;
     CorRosca?: string;
+    ContraCor?: string;
     CorTexto?: string;
 }
