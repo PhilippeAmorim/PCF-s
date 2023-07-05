0aedf1ccc914ac21f9d31761cd7fc27c973f5e67
PhilippeAmorim <65184037+PhilippeAmorim@users.noreply.github.com>
2023-07-05 10:54:36 -0300

    Update ManifestTypes.d.ts

--- Gráfico de Rosca/GraficoDeRosca/generated/ManifestTypes.d.ts
+++ Gráfico de Rosca/GraficoDeRosca/generated/ManifestTypes.d.ts
@@ -5,13 +5,19 @@
 // Define IInputs and IOutputs Type. They should match with ControlManifest.
 export interface IInputs {
     Valor: ComponentFramework.PropertyTypes.DecimalNumberProperty;
+    EspessuraRosca: ComponentFramework.PropertyTypes.DecimalNumberProperty;
     CorRosca: ComponentFramework.PropertyTypes.StringProperty;
     ContraCor: ComponentFramework.PropertyTypes.StringProperty;
     CorTexto: ComponentFramework.PropertyTypes.StringProperty;
+    TamanhoTexto: ComponentFramework.PropertyTypes.DecimalNumberProperty;
+    EspessuraTexto: ComponentFramework.PropertyTypes.EnumProperty<"Negrito" | "Seminegrito" | "Normal" | "Mais Clara">;
 }
 export interface IOutputs {
     Valor?: number;
+    EspessuraRosca?: number;
     CorRosca?: string;
     ContraCor?: string;
     CorTexto?: string;
+    TamanhoTexto?: number;
+    EspessuraTexto?: string;
 }
